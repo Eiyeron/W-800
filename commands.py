032201b77b85218fead86e594b10e1ab26ecf386
Eiyeron Fulmicendii <eiyeron@gmail.com>
2015-08-28 12:47:02 +0200

    Fortune!

--- commands.py
+++ commands.py
@@ -1,8 +1,8 @@
+import os
+import random
 import requests
+import subprocess
 from urllib import parse
-
-def walrii(tg, message):
-    tg.send_sticker(message.chat.id, "BQADBAADMwEAAlthFwM-reg8-6kV6QI")
 
 
 def ddg(tg, message):
@@ -16,12 +16,14 @@ def ddg(tg, message):
     else:
         tg.send_message(message.chat.id, "https://duckduckgo.com/?q="+parse.quote_plus(argument))
 
-def _lastest_xkcd(tg, message):
-    r = requests.get("http://xkcd.com/info.0.json").json()
-    tg.send_message(message.chat.id, "'{} - {} - {}".format(r['title'], r["img"], r["alt"]))
-
+def walrii(tg, message):
+    tg.send_sticker(message.chat.id, "BQADBAADMwEAAlthFwM-reg8-6kV6QI")
 
 def xkcd(tg, message):
+    def _lastest_xkcd(tg, message):
+        r = requests.get("http://xkcd.com/info.0.json").json()
+        tg.send_message(message.chat.id, "'{} - {} - {}".format(r['title'], r["img"], r["alt"]))
+
     array = message.text.split(" ", 1)
     if len(array) < 2:
         lastest_xkcd(tg, message)
@@ -37,3 +39,26 @@ def xkcd(tg, message):
             tg.send_message(message.chat.id, "{} - {} - {}".format(r['title'], r["img"], r["alt"]))
         except ValueError:
             tg.send_message(message.chat.id, "That wasn't a number. EXTERMINATE!")
+
+def fortune(tg, message):
+    # subprocess.check_output('fortune').decode('utf-8')
+    allowed_fortunes = [ f for f in os.listdir("/usr/share/games/fortunes") if '.' not in f]
+    array = message.text.split(" ", 1)
+    if len(array) > 2:
+        print(message.chat.id, "Too much arguments. EXTERMINATE!")
+    elif len(array) == 1:
+        tg.send_message(message.chat.id,
+                        subprocess.check_output(['fortune',
+                                                 random.choice(allowed_fortunes)]
+                                               ).decode('utf-8')
+                       )
+    elif array[1] == '-l' or array[1] == '--list':
+        result = "Here are the fortunes I have, pathetic human : \n"
+        for file in allowed_fortunes:
+            result += file+'\n'
+        tg.send_message(message.chat.id, result)
+    elif array[1] in allowed_fortunes:
+        tg.send_message(message.chat.id, subprocess.check_output(['fortune', array[1]]).decode('utf-8'))
+    else:
+        tg.send_message(message.chat.id, "Invalid fortune. EXTERMINATE")
+
